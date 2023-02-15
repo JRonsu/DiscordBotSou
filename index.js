5056f96b5f9e1a22a8016f0978ffad25c54d40b9
Ronsou <samelsonjohn@protonmail.com>
2023-02-15 09:34:00 -0500

    Server is running

--- index.js
+++ index.js
@@ -44,20 +44,49 @@ client.login(process.env.DISCORD_BOT_TOKEN);
 const options = {
   url: 'https://accounts.spotify.com/api/token',
   headers: {
-    'Authorization': 'Basic ' + (new Buffer(process.env.Spotify_Client_Key + ':' + process.env.Spotify_Secret_Key).toString('base64'))
+  'Authorization': 'Basic ' + (new Buffer(process.env.Spotify_Client_Key + ':' + process.env.Spotify_Secret_Key).toString('base64'))
   },
   form: {
-    grant_type: 'client_credentials'
+  grant_type: 'client_credentials'
   },
   json: true
-};
-
-request.post(options, function(error, response, body) {
+  };
+  
+  let access_token = '';
+  
+  request.post(options, function(error, response, body) {
   if (!error && response.statusCode === 200) {
-    console.log("Connected to Spotify API successfully!")
-    // Store the access token for future use
-    const access_token = body.access_token;
+  console.log("Connected to Spotify API successfully!")
+  // Store the access token for future use
+  access_token = body.access_token;
   } else {
-    console.log("Error connecting to Spotify API:", error)
+  console.log("Error connecting to Spotify API:", error)
+  }
+  });
+  
+// Listen for messages sent to the server
+client.on('message', msg => {
+  // Check if the message content is equal to "current song"
+  if (msg.content === 'current song') {
+    // Make a GET request to the Spotify Web API endpoint for retrieving the current user's playing track
+    request.get({
+      url: 'https://api.spotify.com/v1/me/player/currently-playing',
+      headers: {
+        'Authorization': 'Bearer ' + process.env.Spotify_Client_Keyc
+      },
+      json: true
+    }, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        // Retrieve the track info
+        const track = body.item;
+        const trackName = track.name;
+        const artist = track.artists[0].name;
+
+        // Reply to the message with the current song info
+        msg.reply(`The current song is "${trackName}" by ${artist}.`);
+      } else {
+        console.log("Error retrieving track data:", error);
+      }
+    });
   }
 });
